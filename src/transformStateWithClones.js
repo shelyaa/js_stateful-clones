f417ce37d1a2b5515796ee6977a2e57adea10d11
Aliona <ashelyaa17@gmail.com>
2024-11-29 13:08:28 +0200

    task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,42 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const results = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties': {
+        currentState = {
+          ...currentState,
+          ...action.extraData,
+        };
+        break;
+      }
+
+      case 'removeProperties': {
+        currentState = { ...currentState };
+
+        action.keysToRemove.forEach((key) => {
+          delete currentState[key];
+        });
+        break;
+      }
+
+      case 'clear': {
+        currentState = {};
+        break;
+      }
+
+      default: {
+        throw new Error(`Unknown action type ${action.type}`);
+      }
+    }
+
+    results.push(currentState);
+  }
+
+  return results;
 }
 
 module.exports = transformStateWithClones;
